6aaab61919b05aa9ee16de74c46bb4e5d3f70751
jlegendary <j.legendary2@gmail.com>
2015-08-15 20:04:27 -0400

    Added main.cpp in Problem 1

---
+++ 1 Sum "A+B"/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a,b,c;
+    cout << "Enter two set of digits: ";
+    cin >> a >> b;
+    c= a+b;
+    
+    cout << "Sum of Two: " << c << endl;
+    
+    return 0;
+}
